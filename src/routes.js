51f9c37ba6412856d4b92286882660ec8650a28a
DarlanDuarte <darlan.duarte.m@gmail.com>
2022-12-04 16:33:28 -0300

    Primeiro Commit

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+import Home from "./pages/Home";
+
+
+const Router = () =>{ 
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element = {<Home/>} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Router
