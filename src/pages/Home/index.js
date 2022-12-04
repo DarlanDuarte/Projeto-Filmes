11e0ba319a91f48cbf5ae78eeb6eb5cec3ec2739
DarlanDuarte <darlan.duarte.m@gmail.com>
2022-12-04 20:50:26 -0300

    colocando-api-axios

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,41 @@
 import { Link } from "react-router-dom"
+import React, {useState, useEffect} from "react"
+import api from "../../services/api"
+import './home.css'
 
 const Home   = () =>{
+    const [filme, setFilme] = useState([])
+
+
+    useEffect(()=>{
+
+        async function loadFilmes(){
+            const response = await api.get('/r-api/?api=filmes')
+            //console.log(response.data)
+            setFilme(response.data)
+
+        }
+
+        loadFilmes()
+
+    }, [])
+
+
+
     return(
-        <div>
-            <h1>Pagina Home</h1>
+        <div className="container">
+            <div className="list-filmes">
+                {filme.map((filme)=>{
+                    return(
+                        <article key={filme.id}>
+                            <strong>{filme.nome}</strong>
+                            <img src={filme.foto} alt ={filme.nome} />
+                            <Link to= '/' >Acessar</Link>
+                        </article>
+                    )
+                })}
+
+            </div>
         </div>
     )
 }
